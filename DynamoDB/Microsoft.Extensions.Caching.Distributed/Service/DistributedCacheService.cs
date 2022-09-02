aa92f8b817e4fd069ba685c2ba0a3fca13de5787
Jonatan Rinckus <jonatan@loop3.com.br>
2022-09-02 11:58:47 -0300

    implemented missing methods

--- DynamoDB/Microsoft.Extensions.Caching.Distributed/Service/DistributedCacheService.cs
+++ DynamoDB/Microsoft.Extensions.Caching.Distributed/Service/DistributedCacheService.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Text;
-using System.Threading.Tasks;
-using Amazon.DynamoDBv2.DataModel;
+﻿using Amazon.DynamoDBv2.DataModel;
 using Microsoft.Extensions.Caching.Distributed.DynamoDb.Constants;
 using Microsoft.Extensions.Caching.Distributed.DynamoDb.Manager;
 using Microsoft.Extensions.Caching.Distributed.DynamoDb.Models;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Microsoft.Extensions.Caching.Distributed.DynamoDb.Service
 {
     public class DistributedCacheService<T> : IDistributedCache where T : ICacheTable
-    {   
+    {
         private readonly IDynamoDBContext _dynamoDbContext;
         private readonly ICacheTtlManager _cacheTtlManager;
         private readonly Encoding _encoding;
@@ -20,7 +21,7 @@ namespace Microsoft.Extensions.Caching.Distributed.DynamoDb.Service
             _cacheTtlManager = cacheTtlManager;
 
             _encoding = encoding;
-            
+
             startUpManager.Run(typeof(T).Name);
         }
 
@@ -40,9 +41,25 @@ namespace Microsoft.Extensions.Caching.Distributed.DynamoDb.Service
             return _encoding.GetBytes(_dynamoDbContext.LoadAsync<T>(key).GetAwaiter().GetResult().Value);
         }
 
-        public Task<byte[]> GetAsync(string key)
+        public byte[] Get(string key, CancellationToken token = default)
         {
-            return Task.FromResult(Get(key));
+            var cacheItem = _dynamoDbContext.LoadAsync<T>(key, token).GetAwaiter().GetResult();
+
+            if (cacheItem == null)
+                return null;
+
+            if (cacheItem.Ttl < _cacheTtlManager.ToUnixTime(DateTime.UtcNow))
+            {
+                Remove(cacheItem.CacheId);
+                return null;
+            }
+
+            return _encoding.GetBytes(_dynamoDbContext.LoadAsync<T>(key, token).GetAwaiter().GetResult().Value);
+        }
+
+        public Task<byte[]> GetAsync(string key, CancellationToken token = default)
+        {
+            return Task.FromResult(Get(key, token));
         }
 
         public void Set(string key, byte[] value, DistributedCacheEntryOptions options)
@@ -51,15 +68,27 @@ namespace Microsoft.Extensions.Caching.Distributed.DynamoDb.Service
 
             cacheItem.CacheId = key;
             cacheItem.Value = _encoding.GetString(value);
-            cacheItem.CacheOptions = _cacheTtlManager.ToCacheOptions(options); 
+            cacheItem.CacheOptions = _cacheTtlManager.ToCacheOptions(options);
             cacheItem.Ttl = _cacheTtlManager.ToTtl(cacheItem.CacheOptions);
 
             _dynamoDbContext.SaveAsync(cacheItem).GetAwaiter().GetResult();
         }
 
-        public Task SetAsync(string key, byte[] value, DistributedCacheEntryOptions options)
+        public void Set(string key, byte[] value, DistributedCacheEntryOptions options, CancellationToken token = default)
         {
-            Set(key, value, options);
+            var cacheItem = Activator.CreateInstance<T>();
+
+            cacheItem.CacheId = key;
+            cacheItem.Value = _encoding.GetString(value);
+            cacheItem.CacheOptions = _cacheTtlManager.ToCacheOptions(options);
+            cacheItem.Ttl = _cacheTtlManager.ToTtl(cacheItem.CacheOptions);
+
+            _dynamoDbContext.SaveAsync(cacheItem, token).GetAwaiter().GetResult();
+        }
+
+        public Task SetAsync(string key, byte[] value, DistributedCacheEntryOptions options, CancellationToken token = default)
+        {
+            Set(key, value, options, token);
 
             return Task.FromResult(0);
         }
@@ -76,12 +105,27 @@ namespace Microsoft.Extensions.Caching.Distributed.DynamoDb.Service
                 }
 
                 _dynamoDbContext.SaveAsync(cacheItem).GetAwaiter().GetResult();
-            } 
+            }
         }
 
-        public Task RefreshAsync(string key)
+        public void Refresh(string key, CancellationToken token = default)
         {
-            Refresh(key);
+            var cacheItem = _dynamoDbContext.LoadAsync<T>(key, token).GetAwaiter().GetResult();
+
+            if (cacheItem != null)
+            {
+                if (cacheItem.CacheOptions.Type == CacheExpiryType.Sliding)
+                {
+                    cacheItem.Ttl = _cacheTtlManager.ToTtl(cacheItem.CacheOptions);
+                }
+
+                _dynamoDbContext.SaveAsync(cacheItem, token).GetAwaiter().GetResult();
+            }
+        }
+
+        public Task RefreshAsync(string key, CancellationToken token = default)
+        {
+            Refresh(key, token);
 
             return Task.FromResult(0);
         }
@@ -91,9 +135,14 @@ namespace Microsoft.Extensions.Caching.Distributed.DynamoDb.Service
             _dynamoDbContext.DeleteAsync<T>(key).GetAwaiter().GetResult();
         }
 
-        public Task RemoveAsync(string key)
+        public void Remove(string key, CancellationToken token = default)
         {
-            Remove(key);
+            _dynamoDbContext.DeleteAsync<T>(key, token).GetAwaiter().GetResult();
+        }
+
+        public Task RemoveAsync(string key, CancellationToken token = default)
+        {
+            Remove(key, token);
 
             return Task.FromResult(0);
         }
